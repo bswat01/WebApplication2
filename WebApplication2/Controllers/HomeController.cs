93b87546cbd57c21a787503b9d8c06a1da975a82
Ben <beswat14@gmail.com>
2019-11-22 09:36:37 -0700

    Added Comment

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //I love Professor Anderson
+            return View();
+        }
+    }
+}
